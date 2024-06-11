517989b0ee906cd278bd57029cf86f5c1d9b898d
tejas <tejasftw117@gmail.com>
2024-06-11 18:23:00 +0530

    moved all the pages into (pages) directory

--- src/app/(pages)/signup/page.tsx
+++ src/app/(pages)/signup/page.tsx
@@ -4,7 +4,7 @@
 
 import Image from 'next/image';
 import styles from './signup.module.css';
-import SignupForm from '../components/signupForm/signupForm';
+import SignupForm from '../../components/signupForm/signupForm';
 import Link from 'next/link';
 
 function SignUpPage() {
--- src/app/api/auth/signup/route.ts
+++ src/app/api/auth/signup/route.ts
@@ -73,6 +73,7 @@ export async function POST(req: NextRequest) {
       { status: 200 },
     );
   } catch (error: any) {
+    console.log(error);
     return NextResponse.json(
       { message: 'Internal server error' }, 
       { status: 500 }
