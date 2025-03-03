c75036670157b3715a973ef1ab682b197c89cbe3
Tejas-117 <tejasftw117@gmail.com>
2025-03-03 19:46:31 +0530

    skipping the linting and type checking

--- next.config.mjs
+++ next.config.mjs
@@ -3,12 +3,12 @@ const nextConfig = {
   reactStrictMode: true,
   distDir: 'build',
   output: 'standalone',
-  // eslint: {
-  //   ignoreDuringBuilds: true, // ignore lint check during build
-  // },
-  // typescript: {
-  //   ignoreBuildErrors: true, // ignore type check during build
-  // },
+  eslint: {
+    ignoreDuringBuilds: true, // ignore lint check during build
+  },
+  typescript: {
+    ignoreBuildErrors: true, // ignore type check during build
+  },
 };
 
 export default nextConfig;
